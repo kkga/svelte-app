5c95deb845f589d16284ac0fbadde68f52a3534a
Gadzhi Kharkharov <me@kkga.me>
2021-02-17 02:50:56 +0300

    init

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+  import { lastClicked } from "./stores";
+  import Button from "./Button.svelte";
+
+  function handleMessage(event) {
+    lastClicked.set(event.detail.label);
+  }
+</script>
+
+<main>
+  <h1>{$lastClicked}</h1>
+  <Button on:message={handleMessage} label="1" />
+  <Button on:message={handleMessage} label="2" />
+  <Button on:message={handleMessage} label="3" />
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
---
+++ src/Button.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+
+  export let label = "button";
+
+  function onClick() {
+    dispatch("message", {
+      label: label,
+    });
+  }
+</script>
+
+<button on:click={onClick}>{label}</button>
